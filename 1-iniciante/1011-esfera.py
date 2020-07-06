81f9d654871a5ac148db3f0f9ed8d285be707545
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-07-06 15:27:04 -0300

    Adiciona teste ao 1011

--- 1-iniciante/1011-esfera.py
+++ 1-iniciante/1011-esfera.py
@@ -2,8 +2,13 @@
 
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/1010
 
+def main(raio):
+    volume = float(4/3) * 3.14159 * raio * raio * raio
+    return ('VOLUME = {:.3f}'.format(volume))
+
+assert main(3) == 'VOLUME = 113.097'
+assert main(15) == 'VOLUME = 14137.155'
+assert main(1523) == 'VOLUME = 14797486501.627'
+
 raio = float(input())
-
-volume = float(4/3) * 3.14159 * raio * raio * raio
-
-print('VOLUME = {:.3f}'.format(volume))
+print(main(raio))
